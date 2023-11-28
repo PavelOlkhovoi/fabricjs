0e435fa069240360280aef493811f40c6f2349df
Pavel <p-olhovoi@bk.ru>
2023-11-28 18:02:26 +0100

    Change settings of active item

--- src/stories/simplepallete/AddPalettesToCanvas.stories.jsx
+++ src/stories/simplepallete/AddPalettesToCanvas.stories.jsx
@@ -41,25 +41,96 @@ export const SimplePalette = () => {
   );
 };
 
-export const SimplePaletteWithText = () => {
+export const SimplifiedTestVersion = () => {
   const [canvas, setCanvas] = useState("");
+  const [activeItems, setActiveItems] = useState();
   useEffect(() => {
-    setCanvas(initCanvas());
-  }, []);
-  const initCanvas = () =>
-    new fabric.Canvas("canvas", {
+    const cInstance = new fabric.Canvas("canvas", {
       height: 600,
       width: 1000,
     });
-  const addText = (canvi) => {
+    setCanvas(cInstance);
+    cInstance.on("mouse:down", handleActiveItem);
+  }, []);
+
+  const handleActiveItem = (obj) => {
+    setActiveItems(obj.target);
+  };
+  const handleDeleteActiveItem = () => {
+    canvas.remove(activeItems);
+    canvas.renderAll();
+  };
+
+  const addRect = () => {
     const rect = new fabric.Rect({
       height: 100,
       width: 100,
       fill: "yellow",
     });
-    canvi.add(rect);
+    canvas.add(rect);
+    canvas.renderAll();
+  };
+
+  const enterBtnHandler = (value) => {
+    const text = new fabric.Text(value.target.value, {
+      fontFamily: "helvetica",
+      fontSize: 30,
+      left: 80,
+      top: 100,
+    });
+    canvas.add(text);
+    canvas.renderAll();
+  };
+
+  return (
+    <div style={{ fontFamily: "Helvetica", border: "1px solid, black" }}>
+      <h1>Simple palette example</h1>
+      <div>
+        <Button
+          icon={<PlusOutlined />}
+          onClick={() => addRect(canvas)}
+        ></Button>
+        <Input
+          defaultValue="Some text"
+          style={{ width: "150px", marginLeft: "10px" }}
+          onPressEnter={(e) => enterBtnHandler(e)}
+        />
+        <Button onClick={handleDeleteActiveItem}>Delete</Button>
+      </div>
+      <canvas
+        id="canvas"
+        width={400}
+        height={200}
+        style={{
+          marginTop: "20px",
+          border: "1px solid #d9d9d9",
+          borderRadius: "6px",
+        }}
+      ></canvas>
+    </div>
+  );
+};
+
+export const SimplePaletteWithText = () => {
+  const [canvas, setCanvas] = useState("");
+  const [activeItems, setActiveItems] = useState();
+  useEffect(() => {
+    const cInstance = new fabric.Canvas("canvas", {
+      height: 600,
+      width: 1000,
+    });
+    setCanvas(cInstance);
+    cInstance.on("mouse:down", handleActiveItem);
+  }, []);
+
+  const handleActiveItem = (obj) => {
+    setActiveItems(obj.target);
+  };
+  const handleDeleteActiveItem = (canvi) => {
+    canvas.remove(activeItems);
     canvi.renderAll();
   };
+
   const addRect = (canvi) => {
     const rect = new fabric.Rect({
       height: 100,
@@ -80,6 +151,7 @@ export const SimplePaletteWithText = () => {
     canvi.add(text);
     canvi.renderAll();
   };
+
   return (
     <div style={{ fontFamily: "Helvetica", border: "1px solid, black" }}>
       <h1>Simple palette example</h1>
@@ -93,6 +165,89 @@ export const SimplePaletteWithText = () => {
           style={{ width: "150px", marginLeft: "10px" }}
           onPressEnter={(e) => enterBtnHandler(canvas, e)}
         />
+        <Button onClick={() => handleDeleteActiveItem(canvas)}>Delete</Button>
+      </div>
+      <canvas
+        id="canvas"
+        width={400}
+        height={200}
+        style={{
+          marginTop: "20px",
+          border: "1px solid #d9d9d9",
+          borderRadius: "6px",
+        }}
+      ></canvas>
+    </div>
+  );
+};
+
+export const ChangePositionInStack = () => {
+  const [canvas, setCanvas] = useState("");
+  const [activeItems, setActiveItems] = useState();
+  useEffect(() => {
+    const cInstance = new fabric.Canvas("canvas", {
+      height: 600,
+      width: 1000,
+    });
+    setCanvas(cInstance);
+    cInstance.on("mouse:down", handleActiveItem);
+  }, []);
+
+  const handleActiveItem = (obj) => {
+    setActiveItems(obj.target);
+  };
+  const handleDeleteActiveItem = () => {
+    canvas.remove(activeItems);
+    canvas.renderAll();
+  };
+  const handleMoveUpActiveItem = () => {
+    activeItems.bringToFront();
+  };
+  const handleMoveDownActiveItem = () => {
+    activeItems.sendToBack();
+  };
+  const handleOpacityActiveItem = () => {
+    activeItems.set("opacity", 0.4);
+  };
+
+  const addRect = () => {
+    const rect = new fabric.Rect({
+      height: 100,
+      width: 100,
+      fill: "yellow",
+    });
+    canvas.add(rect);
+    canvas.renderAll();
+  };
+
+  const enterBtnHandler = (value) => {
+    const text = new fabric.Text(value.target.value, {
+      fontFamily: "helvetica",
+      fontSize: 30,
+      left: 80,
+      top: 100,
+    });
+    canvas.add(text);
+    canvas.renderAll();
+  };
+
+  return (
+    <div style={{ fontFamily: "Helvetica", border: "1px solid, black" }}>
+      <h1>Simple palette example</h1>
+      <div>
+        <Button
+          icon={<PlusOutlined />}
+          onClick={() => addRect(canvas)}
+        ></Button>
+        <Input
+          defaultValue="Some text"
+          style={{ width: "150px", marginLeft: "10px" }}
+          onPressEnter={(e) => enterBtnHandler(e)}
+        />
+        <Button onClick={handleDeleteActiveItem}>Delete</Button>
+        <Button onClick={handleMoveUpActiveItem}>Move Up</Button>
+        <Button onClick={handleMoveDownActiveItem}>Move Down</Button>
+        <Button onClick={handleOpacityActiveItem}>Opacity</Button>
       </div>
       <canvas
         id="canvas"
