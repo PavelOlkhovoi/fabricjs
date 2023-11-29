b6b31c040858ce13151c9adb20e56d9290b51ffc
Pavel <p-olhovoi@bk.ru>
2023-11-29 09:18:20 +0100

    Preparing to get and set opacity

--- src/stories/simplepallete/AddPalettesToCanvas.stories.jsx
+++ src/stories/simplepallete/AddPalettesToCanvas.stories.jsx
@@ -1,10 +1,120 @@
 import React, { useEffect, useState } from "react";
 import { fabric } from "fabric";
-import { Button, Input } from "antd";
+import {
+  Button,
+  Input,
+  Flex,
+  ColorPicker,
+  Divider,
+  Col,
+  InputNumber,
+  Row,
+  Slider,
+  Space,
+} from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
 export default {
   title: "Stories/AddPalettesToCanvas",
+};
+
+const colorArr = [
+  "#000000",
+  "#000000E0",
+  "#000000A6",
+  "#00000073",
+  "#00000040",
+  "#00000026",
+  "#0000001A",
+  "#00000012",
+  "#0000000A",
+  "#00000005",
+  "#F5222D",
+  "#FA8C16",
+  "#FADB14",
+  "#8BBB11",
+  "#52C41A",
+  "#13A8A8",
+  "#1677FF",
+  "#2F54EB",
+  "#722ED1",
+  "#EB2F96",
+  "#F5222D4D",
+  "#FA8C164D",
+  "#FADB144D",
+  "#8BBB114D",
+  "#52C41A4D",
+  "#13A8A84D",
+  "#1677FF4D",
+  "#2F54EB4D",
+  "#722ED14D",
+  "#EB2F964D",
+];
+
+const IntegerStep = () => {
+  const [inputValue, setInputValue] = useState(1);
+  const onChange = (newValue) => {
+    setInputValue(newValue);
+  };
+  return (
+    <Row>
+      <Col span={12}>
+        <Slider
+          min={1}
+          max={20}
+          onChange={onChange}
+          value={typeof inputValue === "number" ? inputValue : 0}
+        />
+      </Col>
+      <Col span={4}>
+        <InputNumber
+          min={1}
+          max={20}
+          style={{
+            margin: "0 16px",
+          }}
+          value={inputValue}
+          onChange={onChange}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+const DecimalStep = ({ defaultOpacity = 1, setOpacity }) => {
+  const [inputValue, setInputValue] = useState(defaultOpacity);
+  const onChange = (value) => {
+    if (isNaN(value)) {
+      return;
+    }
+    setInputValue(value);
+    setOpacity(value);
+  };
+  return (
+    <Row>
+      <Col span={6}>
+        <InputNumber
+          min={0}
+          max={1}
+          style={{
+            margin: "0 16px",
+          }}
+          step={0.01}
+          value={inputValue}
+          onChange={onChange}
+        />
+      </Col>
+      <Col span={10}>
+        <Slider
+          min={0}
+          max={1}
+          onChange={onChange}
+          value={typeof inputValue === "number" ? inputValue : 0}
+          step={0.01}
+        />
+      </Col>
+    </Row>
+  );
 };
 
 export const SimplePalette = () => {
@@ -184,6 +294,7 @@ export const SimplePaletteWithText = () => {
 export const ChangePositionInStack = () => {
   const [canvas, setCanvas] = useState("");
   const [activeItems, setActiveItems] = useState();
+  const [opacity, setOpacity] = useState();
   useEffect(() => {
     const cInstance = new fabric.Canvas("canvas", {
       height: 600,
@@ -191,10 +302,14 @@ export const ChangePositionInStack = () => {
     });
     setCanvas(cInstance);
     cInstance.on("mouse:down", handleActiveItem);
+    return () => cInstance.off("mouse:down", handleActiveItem);
   }, []);
 
   const handleActiveItem = (obj) => {
     setActiveItems(obj.target);
+    console.log("xxx opacity", obj.target);
+    // const currentOpacity = obj.target.get("opacity");
+    // setOpacity(currentOpacity);
   };
   const handleDeleteActiveItem = () => {
     canvas.remove(activeItems);
@@ -206,8 +321,11 @@ export const ChangePositionInStack = () => {
   const handleMoveDownActiveItem = () => {
     activeItems.sendToBack();
   };
-  const handleOpacityActiveItem = () => {
-    activeItems.set("opacity", 0.4);
+  const handleOpacityActiveItem = (testOpacity) => {
+    activeItems.set("opacity", testOpacity);
+  };
+  const handleBGColorActiveItem = (value, hex) => {
+    activeItems.set("fill", hex);
   };
 
   const addRect = () => {
@@ -232,23 +350,95 @@ export const ChangePositionInStack = () => {
   };
 
   return (
-    <div style={{ fontFamily: "Helvetica", border: "1px solid, black" }}>
+    <div
+      style={{
+        fontFamily: "Helvetica",
+        border: "1px solid, black",
+        width: "1000px",
+      }}
+    >
       <h1>Simple palette example</h1>
-      <div>
+      <Flex gap="small" align="start" horiznital>
         <Button
           icon={<PlusOutlined />}
           onClick={() => addRect(canvas)}
         ></Button>
+        <ColorPicker
+          onChange={handleBGColorActiveItem}
+          styles={{
+            popupOverlayInner: {
+              width: 468 + 24,
+            },
+          }}
+          presets={[
+            {
+              label: "Recommended",
+              colors: colorArr,
+            },
+            {
+              label: "Recent",
+              colors: [
+                "#F5222D4D",
+                "#FA8C164D",
+                "#FADB144D",
+                "#8BBB114D",
+                "#52C41A4D",
+                "#13A8A84D",
+              ],
+            },
+          ]}
+          panelRender={(_, { components: { Picker, Presets } }) => (
+            <div
+              className="custom-panel"
+              style={{
+                display: "flex",
+                width: 468,
+              }}
+            >
+              <div
+                style={{
+                  flex: 1,
+                }}
+              >
+                <Presets />
+              </div>
+              <Divider
+                type="vertical"
+                style={{
+                  height: "auto",
+                }}
+              />
+              <div
+                style={{
+                  width: 234,
+                }}
+              >
+                <Picker />
+              </div>
+            </div>
+          )}
+        />
         <Input
           defaultValue="Some text"
-          style={{ width: "150px", marginLeft: "10px" }}
+          style={{ width: "150px" }}
           onPressEnter={(e) => enterBtnHandler(e)}
         />
         <Button onClick={handleDeleteActiveItem}>Delete</Button>
         <Button onClick={handleMoveUpActiveItem}>Move Up</Button>
         <Button onClick={handleMoveDownActiveItem}>Move Down</Button>
         <Button onClick={handleOpacityActiveItem}>Opacity</Button>
-      </div>
+        <Space
+          style={{
+            width: "100%",
+          }}
+          direction="vertical"
+        >
+          <DecimalStep
+            setOpacity={handleOpacityActiveItem}
+            defaultOpacity={opacity}
+          />
+        </Space>
+      </Flex>
       <canvas
         id="canvas"
         width={400}
