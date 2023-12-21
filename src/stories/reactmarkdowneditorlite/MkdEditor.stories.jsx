61e19496ba36d3b92b3d7c93f0794c69472e9fd0
Pavel <p-olhovoi@bk.ru>
2023-12-21 08:27:41 +0100

    add a custom menu to mdx edit

--- src/stories/reactmarkdowneditorlite/MkdEditor.stories.jsx
+++ src/stories/reactmarkdowneditorlite/MkdEditor.stories.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import Editor from "react-markdown-editor-lite";
-import MdEditor from "react-markdown-editor-lite";
+import MdEditor, { Plugins } from "react-markdown-editor-lite";
 import ReactMarkdown from "react-markdown";
 import MarkdownIt from "markdown-it";
 import "react-markdown-editor-lite/lib/index.css";
@@ -46,13 +46,66 @@ const mdParser = new MarkdownIt(/* Markdown-it options */);
 function handleEditorChange({ html, text }) {
   console.log("handleEditorChange", html, text);
 }
+function onImageUpload(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = (data) => {
+      resolve(data.target.result);
+    };
+    reader.readAsDataURL(file);
+  });
+}
+// MdEditor.unuse(Plugins.Header);
+// MdEditor.use(Plugins.AutoResize, {
+//   min: 100, // min height
+//   max: 200, // max height
+// });
+
+// const plugins = [
+//   "header",
+//   "image",
+//   "divider",
+//   "link",
+//   "clear",
+//   "divider",
+//   "font-bold",
+// ];
+
+const plugins = [
+  "header",
+  "font-bold",
+  "font-italic",
+  "font-underline",
+  "font-strikethrough",
+  "list-unordered",
+  "list-ordered",
+  "block-quote",
+  "block-wrap",
+  "block-code-inline",
+  "block-code-block",
+  "table",
+  "image",
+  "link",
+  "clear",
+  "logger",
+  "mode-toggle",
+  "full-screen",
+  "tab-insert",
+];
 
 export const MkdEditorWithMarkDownIt = () => {
+  const [value, setValue] = React.useState("Text from use state");
   return (
-    <MdEditor
-      style={{ height: "500px" }}
-      renderHTML={(text) => mdParser.render(text)}
-      onChange={handleEditorChange}
-    />
+    <div>
+      <MdEditor
+        pl
+        style={{ height: "500px" }}
+        // value={value}
+        plugins={plugins}
+        renderHTML={(text) => mdParser.render(text)}
+        onChange={handleEditorChange}
+        onImageUpload={onImageUpload}
+      />
+    </div>
   );
 };
