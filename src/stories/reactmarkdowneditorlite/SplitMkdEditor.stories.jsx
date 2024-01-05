d22ceb312b8d21ff3bc3025d4afad00140cf6117
Pavel <p-olhovoi@bk.ru>
2024-01-05 18:18:22 +0100

    Style mode view buttons

---
+++ src/stories/reactmarkdowneditorlite/SplitMkdEditor.stories.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import MdEditor, { Plugins } from "react-markdown-editor-lite";
+import MarkdownIt from "markdown-it";
+import "react-markdown-editor-lite/lib/index.css";
+import "./splitstyle.css";
+export default {
+  title: "MKEditorSlpit/MkdEditorEditor",
+};
+
+const mdParser = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+});
+
+MdEditor.addLocale("de-DE", {
+  btnHeader: "Überschrift",
+  btnClear: "Löschen",
+  btnBold: "Fett",
+});
+MdEditor.useLocale("de-DE");
+
+const ViewMode = (props) => {
+  console.log("xxx ViewMode", props.editor.config.canView);
+  const [isWriteActive, setIsWriteActive] = useState(true);
+  const [isPreviewActive, setIsPreviewActive] = useState(false);
+  const handleClickPreview = () => {
+    props.editor.setView({
+      md: false,
+      menu: true,
+      html: true,
+    });
+    setIsWriteActive(false);
+    setIsPreviewActive(true);
+  };
+  const handleClickWrite = () => {
+    props.editor.setView({
+      md: true,
+      menu: true,
+      html: false,
+    });
+    setIsWriteActive(true);
+    setIsPreviewActive(false);
+  };
+
+  return (
+    <div>
+      <button
+        className={isWriteActive ? "mode-btn-toogle-active" : "mode-btn-toogle"}
+        onClick={handleClickWrite}
+        title="Schreibmodus"
+      >
+        Write
+      </button>
+      <button
+        className={
+          isPreviewActive ? "mode-btn-toogle-active" : "mode-btn-toogle"
+        }
+        onClick={handleClickPreview}
+        title="Vorschaumodus"
+      >
+        Preview
+      </button>
+    </div>
+  );
+};
+
+ViewMode.defaultConfig = {};
+ViewMode.align = "left";
+ViewMode.pluginName = "viewmode";
+
+MdEditor.use(ViewMode, {});
+
+const pluginsList = [
+  "viewmode",
+  "divider",
+  "logger",
+  "divider",
+  "header",
+  "font-bold",
+  "font-italic",
+  "font-underline",
+  "font-strikethrough",
+  "list-unordered",
+  "list-ordered",
+  "block-quote",
+  "image",
+  "link",
+  "full-screen",
+];
+function handleEditorChange({ html, text }) {
+  console.log("handleEditorChange", html, text);
+}
+export const MkdSplitedEditor = () => {
+  const demoValue = `
+  * text
+  * text
+  * text
+  * 
+  1. text
+  2. text
+  3. text
+
+`;
+  return (
+    <div>
+      <MdEditor
+        style={{ height: "500px" }}
+        plugins={pluginsList}
+        renderHTML={(text) => mdParser.render(text)}
+        value={demoValue}
+        onChange={handleEditorChange}
+        // onImageUpload={onImageUpload}
+        shortcuts={true}
+        view={{ menu: true, md: true, html: false }}
+      />
+    </div>
+  );
+};
